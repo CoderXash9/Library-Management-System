3b19c4b9ccafc2abd660418bc4a49fd217e386c3
CoderXash9 <asahwiniprohit@gmail.com>
2026-08-02 01:15:40 +0530

    added serializer for author

--- library/serializers.py
+++ library/serializers.py
@@ -1,2 +1,9 @@
 from rest_framework import serializers
 from .models import Author, Member, Book, IssueRecord
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Author
+        fields = "__all__"
