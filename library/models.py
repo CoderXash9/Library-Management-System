7e82a7f4c361c64439d341829c31081d2329a765
CoderXash9 <asahwiniprohit@gmail.com>
2026-08-01 20:37:23 +0530

    added models for book

--- library/models.py
+++ library/models.py
@@ -21,3 +21,25 @@ class Member(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    GENRE_CHOICES = [
+        ("Programming", "Programming"),
+        ("Science", "Science"),
+        ("History", "History"),
+        ("Novel", "Novel"),
+        ("Biography", "Biography"),
+    ]
+
+    title = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=20, unique=True)
+    genre = models.CharField(max_length=50, choices=GENRE_CHOICES)
+    published_date = models.DateField()
+    total_copies = models.PositiveIntegerField()
+    available_copies = models.PositiveIntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
