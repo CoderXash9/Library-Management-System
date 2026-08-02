2d82d08384801e1cb4fc2652728d021885647472
CoderXash9 <asahwiniprohit@gmail.com>
2026-08-02 15:46:26 +0530

    added remaing views

--- library/views.py
+++ library/views.py
@@ -1,9 +1,29 @@
 from rest_framework import viewsets
 
-from .models import Author
-from .serializers import AuthorSerializer
+from .models import Author, Member, Book, IssueRecord
+from .serializers import (
+    AuthorSerializer,
+    MemberSerializer,
+    BookSerializer,
+    IssueRecordSerializer,
+)
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class IssueRecordViewSet(viewsets.ModelViewSet):
+    queryset = IssueRecord.objects.all()
+    serializer_class = IssueRecordSerializer
