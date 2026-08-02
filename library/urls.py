d0e95602f37d1f9c9e94cfcc7efeff4d8b6b2c2c
CoderXash9 <asahwiniprohit@gmail.com>
2026-08-02 15:50:03 +0530

    registering all viewsets in urls

--- library/urls.py
+++ library/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import AuthorViewSet
+from .views import (
+    AuthorViewSet,
+    MemberViewSet,
+    BookViewSet,
+    IssueRecordViewSet,
+)
 
 router = DefaultRouter()  # creating the object for the routers
 
 router.register("authors", AuthorViewSet)
+router.register("members", MemberViewSet)
+router.register("books", BookViewSet)
+router.register("IssueRecords", IssueRecordViewSet)
 
 urlpatterns = [path("", include(router.urls))]
