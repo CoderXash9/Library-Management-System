8f0add0e0e9de036e3fef9654a24807ed05a8b09
CoderXash9 <asahwiniprohit@gmail.com>
2026-08-04 00:16:03 +0530

    added url paths

--- library_management/urls.py
+++ library_management/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("library.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("library.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+]
