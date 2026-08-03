9db83608f5f503416f1ff782e8857411b79e62e9
CoderXash9 <asahwiniprohit@gmail.com>
2026-08-04 00:08:47 +0530

    added authectication imports

--- library/views.py
+++ library/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Author, Member, Book, IssueRecord
 from .serializers import (
