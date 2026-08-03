b664b2ebb0b172df06422703ed398299d9fae090
CoderXash9 <asahwiniprohit@gmail.com>
2026-08-04 00:26:42 +0530

    added book serialization validation

--- library/serializers.py
+++ library/serializers.py
@@ -28,3 +28,19 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = "__all__"
+
+    def validate(self, data):
+
+        total = data.get("total_copies")
+        available = data.get("available_copies")
+
+        if total is not None and available is not None:
+            if available > total :
+                raise serializers.ValidationError(
+                    {
+                        "available_copies":
+                        "Available copies cant be more than total copies."
+                    }
+                )
+
+        return data
